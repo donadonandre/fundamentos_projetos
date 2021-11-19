2fba2128caa0852aea14c8c009635a71fc80c5d9
Andre Donadon <andre.donadon@outlook.com>
2021-11-19 16:14:42 -0300

    Testando modulo principal

---
+++ area_circunferencia_v4.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+#import math
+from math import pi
+
+if __name__ == '__main__':
+    # print('pi = ', math.pi)
+    print('pi = ', pi)
+    raio = input('Informe o raio: ')
+
+    print('A área do círculo é: ', pi * float(raio) ** 2)
