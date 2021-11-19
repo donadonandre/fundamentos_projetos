f17b30d22951bcc4630ab7d3fffcbe3b7fd1448f
Andre Donadon <andre.donadon@outlook.com>
2021-11-19 16:32:21 -0300

    Saindo do programa quando o help for chamado com errno

--- area_circunferencia_v10.py
+++ area_circunferencia_v10.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import errno
 from math import pi
 import sys
 
@@ -16,6 +17,8 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         help()
         sys.exit(1)
+        # ou
+        # sys.exit(errno.EPERM)
     else:
         print(sys.argv[0])
         print(sys.argv[1])
